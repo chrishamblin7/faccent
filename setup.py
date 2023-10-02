51316ebfc18c524482a505bfe8a5176519f0bb58
Chris Hamblin <chrishamblin7@gmail.com>
2023-10-02 13:54:15 -0400

    added

---
+++ setup.py
@@ -0,0 +1,9 @@
+#running 'pip install -e .' in the top folder looks at this script. 
+# It enables one to import various python scripts throughout this project as python modules 
+
+from setuptools import setup, find_packages
+
+setup(name='faccent', version='1.0', packages=find_packages())
+
+
+
